7f5d0ee5aaf97e99170e13ca317e911fccf10b79
karart1981 <karapart1981@gmail.com>
2025-06-20 17:24:11 +0400

    I added our projects section in home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
  import OurPartners from "@/components/partners/Partners";
 import Hero from "../components/hero/Hero";
-
+import AboutProjects from "../components/aboutProjects/AboutProjects"
 export default function Home() {
   return (
     <>
       <Hero />
+      <AboutProjects />
       <OurPartners />
     </>
   );
---
+++ src/components/aboutProjects/AboutProjects.tsx
@@ -0,0 +1,60 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+
+type Project = {
+  id: number;
+  title: string;
+  description: string;
+  image: string;
+};
+
+const Projects = () => {
+  const [projects, setProjects] = useState<Project[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/projects')
+      .then((res) => res.json())
+      .then((data) => setProjects(data));
+  }, []);
+
+  return (
+    <div className="py-10 px-6 md:px-20 bg-[#26466e]">
+      <h2 className="text-[55px] font-bold mb-12 text-center text-white leading-[100%]">
+        900 residential projects
+      </h2>
+      <p></p>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+        {projects.map((project) => (
+          <div
+            key={project.id}
+            className="bg-white shadow-lg rounded-2xl overflow-hidden"
+          >
+            <Image
+              width={400}
+              height={240}
+              src={project.image}
+              alt={project.title}
+              className="w-full h-60 object-cover"
+            />
+            <div className="p-4">
+              <h3 className="text-xl font-semibold text-gray-800">
+                {project.title}
+              </h3>
+              <p className="text-gray-600 mt-2">{project.description}</p>
+            </div>
+          </div>
+        ))}
+        <div className="flex items-center bg-white py-4 px-8 rounded-2xl">
+           <Link className="text-white m-auto text-[22px] tracking-[2px]" href="/projects">MORE</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
+
