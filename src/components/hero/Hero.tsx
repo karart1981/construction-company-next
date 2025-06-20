116ad7fdfe5ade7c4ff7a4fcd5c3e422aeb901c0
karart1981 <karapart1981@gmail.com>
2025-06-20 15:41:00 +0400

    I added a favicon for my project

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -6,7 +6,7 @@ const Hero: React.FC = () => {
     <div className="flex flex-col md:flex-row items-center w-full bg-[#eef3fb]">
       <div className="flex flex-col justify-center px-6 py-12 md:px-[100px] md:py-0 md:w-1/2">
         <h5 className="text-[24px] md:text-[30px] tracking-[2px] md:tracking-[3px] pb-4 md:pb-[30px]">
-          Core Values
+          Real Construction
         </h5>
         <h1 className="text-[36px] md:text-[60px] lg:text-[80px] font-extrabold leading-tight md:leading-[120%] border-b-8 md:border-b-[15px] border-[#28466e] pb-4 md:pb-[30px]">
           Building a Better World
