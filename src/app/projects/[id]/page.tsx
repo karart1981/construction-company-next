548a8a23a25bfdb9c41e27a41df5f9bc1b8c0a72
karart1981 <karapart1981@gmail.com>
2025-07-04 12:33:35 +0400

    I changed some styles in contact page

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -189,7 +189,6 @@ export default function ProjectDetailPage() {
         </div>
       </div>
 
-      {/* ✅ Modal */}
       {showModal && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
           <div className="bg-white rounded-lg p-6 w-full max-w-md">
